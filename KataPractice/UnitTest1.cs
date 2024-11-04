90591a8b371051eda0c9f5cde883f6256ddaec8b
Ryan Robbins <rrobbinsHYL@unither.com>
2024-11-04 13:26:06 -0500

    starting point

---
+++ KataPractice/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace KataPractice
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
