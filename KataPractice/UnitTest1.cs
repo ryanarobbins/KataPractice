a595a8d564620778b55dccbe2badfd2be4b38c6b
Jake Poteat <jake.poteat@amwins.com>
2024-11-08 14:13:56 -0500

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:KataPractice/UnitTest1.cs

--- KataPractice/UnitTest1.cs
+++ KataPractice/UnitTest1.cs
@@ -5,8 +5,9 @@ namespace KataPractice
     // Ryan
     // Leah
     // Carolina
-    // Brandon 
     // Jake
+    // Brandon 
+
     public class Tests
     {
         [SetUp]
@@ -18,10 +19,18 @@ namespace KataPractice
         public void Test1()
         {
           string number = FizzTranslator(1);
-          Assert.AreEqual(number, "1");
+          Assert.That(number, Is.EqualTo("1"));
         }
 
-        private string FizzTranslator(int i)
+        [Test]
+        public void Test2()
+        {
+          string number = FizzTranslator(2);
+          Assert.That(number, Is.EqualTo("2"));
+        }
+
+
+        private string FizzTranslator(int toTranslate)
         {
           return "1";
         }
