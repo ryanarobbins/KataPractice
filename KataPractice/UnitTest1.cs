0b45082119c80abeafcbe217b26b66d7af9a1806
Leah Mooney <leah.mooney@amwins.com>
2024-11-08 14:33:54 -0500

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:KataPractice/UnitTest1.cs

--- KataPractice/UnitTest1.cs
+++ KataPractice/UnitTest1.cs
@@ -2,49 +2,46 @@ using System.Globalization;
 
 namespace KataPractice
 {
-    // Ryan
-    // Leah
-    // Carolina
-    // Jake
-    // Brandon 
+  // Ryan
+  // Leah
+  // Carolina
+  // Jake
+  // Brandon 
 
-    public class Tests
+  public class Tests
+  {
+    [SetUp]
+    public void Setup()
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void Test1()
-        {
-          string number = FizzTranslator(1);
-          Assert.That(number, Is.EqualTo("1"));
-        }
-
-        [Test]
-        public void Test2()
-        {
-          string number = FizzTranslator(2);
-          Assert.That(number, Is.EqualTo("2"));
-        }
-
-        [Test]
-        public void Test3()
-        {
-            string number = FizzTranslator(4);
-            Assert.That(number, Is.EqualTo("4"));
-        }
-
-
-        private string FizzTranslator(int toTranslate)
-        {
-            if (toTranslate == 2)
-            {
-                return "2";
-            }
-
-          return "1";
-        }
     }
+
+    [Test]
+    [TestCase(1,"1")]
+    [TestCase(1, "1")]
+    public void Test1(int num, string expected)
+    {
+      string number = FizzTranslator(num);
+      Assert.That(number, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test2()
+    {
+      string number = FizzTranslator(2);
+      Assert.That(number, Is.EqualTo("2"));
+    }
+
+    [Test]
+    public void Test3()
+    {
+      string number = FizzTranslator(4);
+      Assert.That(number, Is.EqualTo("4"));
+    }
+
+
+    private string FizzTranslator(int toTranslate)
+    {
+      return toTranslate.ToString();
+    }
+  }
 }
