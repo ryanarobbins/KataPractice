3c3824e92fde66638a85cd9bede37d29dbd0e3f7
Jake Poteat <jake.poteat@amwins.com>
2024-11-08 15:09:30 -0500

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:KataPractice/UnitTest1.cs

--- KataPractice/UnitTest1.cs
+++ KataPractice/UnitTest1.cs
@@ -14,8 +14,10 @@ namespace KataPractice
         [Test]
         [TestCase(1, "1")]
         [TestCase(2, "2")]
-        [TestCase(4, "4")]
         [TestCase(3, "Fizz")]
+        [TestCase(4, "4")]
+        [TestCase(5, "Buzz")]
+        [TestCase(6, "Fizz")]   
         public void TestFizzTranslator_IntegerToString(int num, string expected)
         {
             string number = FizzTranslator(num);
@@ -25,6 +27,16 @@ namespace KataPractice
 
         static string FizzTranslator(int toTranslate)
         {
+            if(toTranslate % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if(toTranslate == 5)
+            {
+                return "Buzz";
+            }
+
             return toTranslate.ToString();
         }
     }
