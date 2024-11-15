a833011e4c985e21d09236258bd7ae95d04fb9c4
Jake Poteat <jake.poteat@amwins.com>
2024-11-15 14:06:43 -0500

    finished kata first draft

--- KataPractice/UnitTest1.cs
+++ KataPractice/UnitTest1.cs
@@ -5,25 +5,57 @@ namespace KataPractice
     // Ryan
     // Leah
     // Carolina
-    // Brandon 
     // Jake
+    // Brandon 
+
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
 
         [Test]
-        public void Test1()
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(4, "4")]
+        [TestCase(5, "Buzz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(15, "FizzBuzz")]
+
+        public void TestFizzTranslator_IntegerToString(int num, string expected)
         {
-          string number = FizzTranslator(1);
-          Assert.AreEqual(number, "1");
+            string number = FizzTranslator(num);
+            Assert.That(number, Is.EqualTo(expected));
         }
 
-        private string FizzTranslator(int i)
+
+        static string FizzTranslator(int toTranslate)
         {
-          return "1";
+            if (ShouldFizz(toTranslate) & ShouldBuzz(toTranslate))
+            {
+                return "FizzBuzz";
+            }
+
+            if (ShouldFizz(toTranslate))
+            {
+                return "Fizz";
+            }
+
+            if (ShouldBuzz(toTranslate))
+            {
+                return "Buzz";
+            }
+
+            return toTranslate.ToString();
+        }
+
+        private static bool ShouldBuzz(int toTranslate)
+        {
+            return toTranslate % 5 == 0;
+        }
+
+        private static bool ShouldFizz(int toTranslate)
+        {
+            return toTranslate % 3 == 0;
         }
     }
 }
