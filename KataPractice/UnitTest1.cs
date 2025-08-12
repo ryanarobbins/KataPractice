7a2d18e4724fb743f3ff6b0ec516aea164413e79
Ryan Robbins <rrobbinsHYL@unither.com>
2025-08-12 16:09:33 -0400

    reset again

--- KataPractice/UnitTest1.cs
+++ KataPractice/UnitTest1.cs
@@ -1,61 +1,12 @@
-using System.Globalization;
-
 namespace KataPractice
 {
-    // Ryan
-    // Leah
-    // Carolina
-    // Jake
-    // Brandon 
-
+    
     public class Tests
     {
-
         [Test]
-        [TestCase(1, "1")]
-        [TestCase(2, "2")]
-        [TestCase(3, "Fizz")]
-        [TestCase(4, "4")]
-        [TestCase(5, "Buzz")]
-        [TestCase(10, "Buzz")]
-        [TestCase(6, "Fizz")]
-        [TestCase(15, "FizzBuzz")]
-
-        public void TestFizzTranslator_IntegerToString(int num, string expected)
+        public void Test()
         {
-            string number = FizzTranslator(num);
-            Assert.That(number, Is.EqualTo(expected));
-        }
-
-
-        static string FizzTranslator(int toTranslate)
-        {
-            if (ShouldFizz(toTranslate) & ShouldBuzz(toTranslate))
-            {
-                return "FizzBuzz";
-            }
-
-            if (ShouldFizz(toTranslate))
-            {
-                return "Fizz";
-            }
-
-            if (ShouldBuzz(toTranslate))
-            {
-                return "Buzz";
-            }
-
-            return toTranslate.ToString();
-        }
-
-        private static bool ShouldBuzz(int toTranslate)
-        {
-            return toTranslate % 5 == 0;
-        }
-
-        private static bool ShouldFizz(int toTranslate)
-        {
-            return toTranslate % 3 == 0;
+            Assert.True(false);
         }
     }
 }
