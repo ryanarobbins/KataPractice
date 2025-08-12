808308043e23cf5f1c496b610a0498198bc396d2
Ryan Robbins <rrobbinsHYL@unither.com>
2025-08-12 17:09:08 -0400

    reset

--- KataPractice/UnitTest1.cs
+++ KataPractice/UnitTest1.cs
@@ -1,46 +1,11 @@
 namespace KataPractice
 {
-
     public class Tests
     {
-        public string Fizzbuzz(int number)
-        {
-            if (number == 3 || number == 6)
-            {
-                return "Fizz";
-            }
-            return number.ToString();
-        }
-
         [Test]
         public void Test()
-        {
-            var result = Fizzbuzz(1);
-
-            Assert.That(result, Is.EqualTo("1"));
-        }
-
-        [Test]
-        public void Test2()
-        {
-            var result = Fizzbuzz(2);
-
-            Assert.That(result, Is.EqualTo("2"));
-        }
-
-        [Test]
-        public void Test3()
-        {
-            var result = Fizzbuzz(3);
-
-            Assert.That(result, Is.EqualTo("Fizz"));
-        }
-
-        [Test]
-        public void Test6()
-        {
-            var result = Fizzbuzz(6);
-            Assert.That(result, Is.EqualTo("Fizz"));
-        }
+        {    
+            Assert.True(false);
+        }        
     }
 }
